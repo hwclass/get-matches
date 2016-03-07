d0bec3db998f9155437d05abfb4c706848ee19e7
hwclass <harderworking.class@gmail.com>
2016-03-07 11:53:45 +0200

    added the main usage in getMatches.js

--- getMatches.js
+++ getMatches.js
@@ -9,4 +9,32 @@ console.log('geo: ' + argv.geo);
 console.log('gender: ' + argv.gender);
 console.log('preferences: ' + argv.preferences);
 
+var Arguments = {
+   build : function (args) {
+      return {
+         id: args.id,
+         name: args.name,
+         country_code: args.country_code,
+         latitude: args.latitude,
+         longitude: args.longitude,
+         gender: args.gender,
+         preferences: args.preferences
+   }
+};
+
+var Query = {
+   build : function (args, cb) {
+      //make the query for db and get result
+      cb(result);
+   }
+};
+
+//usage
+/*
+var args = Arguments.build({country_code : argv.country, geo : argv.geo, gender : argv.gender, preferences : argv.preferences});
+Query.build(args, function (data) {
+   console.dir(data);
+});
+*/
+
 //--country=nl --geo=52.3650172,4.8375675 --gender=F --preferences=fridge,ironing
